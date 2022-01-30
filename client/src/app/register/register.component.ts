f685aac56dddced6ecde18ae3efd9525cefbd411
Satheesh Kumar <s.satheeshkumar2310@gmail.com>
2022-01-30 17:32:18 +0530

    Extending the API

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -22,6 +22,7 @@ export class RegisterComponent implements OnInit {
       this.cancel();
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
       this.toastr.error(error);
     })
   }
