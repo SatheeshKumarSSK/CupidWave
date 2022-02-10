d63c9e4ef4b244fdaa64844feb2bf04bbbe3d4f1
Satheesh Kumar <s.satheeshkumar2310@gmail.com>
2022-02-10 16:14:32 +0530

    Photo Upload Functionality

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -42,20 +42,5 @@ namespace API.Services
 
             return tokenHandler.WriteToken(token);
         }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
     }
 }
